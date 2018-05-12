3d78cbebf50ccc30c24a92df13b5af87facbe93e
t.takahari <taiga1018@gmail.com>
2018-05-13 01:12:18 +0900

    add config.

---
+++ Benchmarks/Configurations/BenchmarkConfig.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmarks.Configurations
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MemoryDiagnoser.Default);
+
+            Add(Job.ShortRun, Job.Core);
+            Add(Job.ShortRun, Job.Clr);
+        }
+    }
+}
